ea5c94e5f934cd9a8bd5fd80fa30bc55fa5f554d
Sebastián <BICHOS1506@GMAIL.COM>
2025-04-15 17:41:25 +0000

    casi terminado

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import "./assets/img/4geeks.ico";
 
 
 let cardContainer = document.querySelector('.cardContainer');
+let drawButton = document.querySelector('#drawButton');
 
 let cardsData = [];
 
@@ -75,6 +76,7 @@ function valorNumerico(carta) {
 }
 
 
+
 drawButton.onclick = function () {
   const user = document.getElementById("user");
   const value = parseInt(user.value);
@@ -92,7 +94,7 @@ drawButton.onclick = function () {
   }
 
 };
-cardsData.forEach(card => addElement(card.numero, card.icon));
+
 
 
 function bubbleSort(arr) {
@@ -106,9 +108,11 @@ function bubbleSort(arr) {
         swapped = true;
       }
     }
-    wall--;
+    wall--; 
   } while (swapped);
 }
+
+
 
 
 sortButton.onclick = function () {
